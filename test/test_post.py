b1d6cb8e812eb41b898d20f21b0a2c092cc3db7f
Donald Futers <donald.futers@gmail.com>
2025-04-15 13:29:38 +0100

    ✅ specify tests for post.py

---
+++ test/test_post.py
@@ -0,0 +1,15 @@
+# import statements
+
+# test_entry_ids_follow_datetime_entry_num_pattern
+
+# test_entry_ids_in_sequence
+
+# test_messages_in_original_sequence
+
+# test_entry_message_bodies_formed_without_loss
+
+# test_max_10_messages_posted
+
+# test_posted_messages_present_in_SQS_queue
+
+# test_SQS_queue_stores_messages_without_loss
