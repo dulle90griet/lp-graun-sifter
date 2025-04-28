185ee7798581d69797dfd532b329f53253775ee3
Donald Futers <donald.futers@gmail.com>
2025-04-28 12:06:24 +0100

    ✅ define and pass third test on gather()

--- test/test_gather.py
+++ test/test_gather.py
@@ -39,18 +39,18 @@ def test_api_key():
 def test_fetch_invoked_once_with_given_search_and_date(sqs, test_api_key):
     queue_url = sqs.create_queue(QueueName="test-sqs-queue")["QueueUrl"]
 
-    with patch('src.lp_graun_sifter.__init__.fetch', wraps=fetch) as fetch_spy:
+    with patch("src.lp_graun_sifter.__init__.fetch", wraps=fetch) as fetch_spy:
         gather(sqs, queue_url, "test search")
         fetch_spy.assert_called_once_with("test search", None)
 
-    with patch('src.lp_graun_sifter.__init__.fetch', wraps=fetch) as fetch_spy:
-        gather(sqs, queue_url, "\"lord byron\"", "1812-03-03")
-        fetch_spy.assert_called_once_with("\"lord byron\"", "1812-03-03")
+    with patch("src.lp_graun_sifter.__init__.fetch", wraps=fetch) as fetch_spy:
+        gather(sqs, queue_url, '"lord byron"', "1812-03-03")
+        fetch_spy.assert_called_once_with('"lord byron"', "1812-03-03")
 
 
 def test_post_invoked_with_results_matching_search(sqs, test_api_key):
     # Define a helper function to keep this test DRY
-    def run_tests():
+    def make_assertions():
         # Test for invocation
         post_spy.assert_called_once()
 
@@ -60,34 +60,53 @@ def test_post_invoked_with_results_matching_search(sqs, test_api_key):
         posted_results = post_spy.call_args.args[2]
         keyword_present_in_all_results = True
         for result in posted_results:
-            query = result["webUrl"].replace(
-                "www.theguardian.com",
-                "content.guardianapis.com"
-            ) + f"?api-key={os.environ["GUARDIAN_API_KEY"]}&show-fields=all"
+            query = (
+                result["webUrl"].replace(
+                    "www.theguardian.com", "content.guardianapis.com"
+                )
+                + f"?api-key={os.environ["GUARDIAN_API_KEY"]}&show-fields=all"
+            )
             response = requests.get(query, timeout=5)
-            fields = response.json()['response']['content']['fields']
+            fields = response.json()["response"]["content"]["fields"]
 
             if not re.search(search_pattern, str(fields).lower()):
                 keyword_present_in_all_results = False
                 break
-        
+
         assert keyword_present_in_all_results
 
     queue_url = sqs.create_queue(QueueName="test-sqs-queue")["QueueUrl"]
 
-    with patch('src.lp_graun_sifter.__init__.post', wraps=post) as post_spy:
+    with patch("src.lp_graun_sifter.__init__.post", wraps=post) as post_spy:
         gather(sqs, queue_url, "grimsby diner")
         search_pattern = r"(grimsby|diner)"
-        run_tests()
-        
-    with patch('src.lp_graun_sifter.__init__.post', wraps=post) as post_spy:
-        gather(sqs, queue_url, "new model announce \"MacBook Pro\"")
+        make_assertions()
+
+    with patch("src.lp_graun_sifter.__init__.post", wraps=post) as post_spy:
+        gather(sqs, queue_url, 'new model announce "MacBook Pro"')
         search_pattern = r"(new|model|announce|macbook pro)"
-        run_tests()
+        make_assertions()
 
 
-# def test_valid_response_dict_received():
-#     # assert response type is dict
-#     # assert contains 'Successful' or 'Failed'
-#     # assert num of Successful/Failed messages sums to total fetched
-#     # assert contains 'ResponseMetaData' with 'HTTPStatusCode'
+def test_valid_response_dict_received(sqs, test_api_key):
+    # Define a helper function to keep this test DRY
+    def make_assertions():
+        assert isinstance(response, dict)
+        assert "Successful" in response or "Failed" in response
+        messages_in_response = len(response.get("Successful"))
+        messages_in_response += len(response.get("Failed"))
+        assert messages_in_response == len(fetched_results)
+        assert "ResponseMetadata" in response
+        assert "HTTPStatusCode" in response["ResponseMetadata"]
+
+    queue_url = sqs.create_queue(QueueName="test-sqs-queue")["QueueUrl"]
+
+    with patch("src.lp_graun_sifter.__init__.post", wraps=post) as post_spy:
+        response = gather(sqs, queue_url, "new development")
+        fetched_results = post_spy.call_args.args[2]
+        make_assertions()
+
+    with patch("src.lp_graun_sifter.__init__.post", wraps=post) as post_spy:
+        response = gather(sqs, queue_url, "Apple announce", "2025-01-01")
+        fetched_results = post_spy.call_args.args[2]
+        make_assertions()
