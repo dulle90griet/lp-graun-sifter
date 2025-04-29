ddc9e9f5e424d2aff51fc534807c600644453662
Donald Futers <donald.futers@gmail.com>
2025-04-29 18:53:34 +0100

    📝 add docstrings to __init__.py
    ✨ include dict of fetched articles from fetch() in return value of gather()

--- src/lp_graun_sifter/__init__.py
+++ src/lp_graun_sifter/__init__.py
@@ -1,3 +1,5 @@
+"""Provides gather(), the interface for the module's integrated function."""
+
 import os
 import boto3
 
@@ -5,9 +7,36 @@ from .fetch import fetch
 from .post import post
 
 
-def gather(sqs_client, sqs_queue_url, search_string, date_from=None) -> dict:
+def gather(sqs_client: boto3.client, sqs_queue_url: str, search_string: str, date_from: str=None) -> dict:
+    """Fetches results from the Guardian API and sends them to an SQS queue.
+    
+    Args:
+        sqs_client:
+            The boto3 client to be used to connect to SQS.
+        sqs_queue_url:
+            The string of the target SQS queue's URL.
+        search_string:
+            The search string to be provided to the Guardian API.
+        date_from (optional):
+            A string in the format "YYYY-MM-DD". If provided, only results
+            later than this date will be requested from the API.
+    
+    Returns:
+        A dict containing:
+        - "Fetched":
+            A list of articles fetched. Each article is represented by a dict
+            with "webPublicationDate", "webTitle", "webUrl" and "contentPreview" keys.
+        - Response data received from SQS:
+            At present, this comprises the keys "Successful", "Failed", and
+            "ResponseMetaData". For more information, see https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/sqs/client/send_message_batch.html.
+        
+        Returned IDs of successful posts to SQS end in a number from 0 to 9.
+        These numbers correspond to the indices of items in the "Fetched" list.
+    """
+
     fetch_results = fetch(search_string, date_from)
     response = post(sqs_client, sqs_queue_url, fetch_results)
+    response.update({"Fetched": fetch_results})
     return response
 
 
