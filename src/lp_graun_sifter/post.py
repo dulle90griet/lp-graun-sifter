eb2d869ff1c1afb21cd98a65de9addf46f2b25d0
Donald Futers <donald.futers@gmail.com>
2025-04-30 13:13:55 +0100

    📝 add docstrings to post.py

--- src/lp_graun_sifter/post.py
+++ src/lp_graun_sifter/post.py
@@ -1,3 +1,5 @@
+"""Defines the post() function."""
+
 from datetime import datetime, UTC
 import sys
 from copy import deepcopy
@@ -6,14 +8,25 @@ import json
 import boto3
 
 
-def post(client, queue_url: str, messages: list[dict]) -> None:
-    """
-    client:
-        The botocore.client.SQS instance to use to connect to AWS SQS.
-    queue_url:
-        The HTTPS endpoint of the SQS queue.
-    messages:
-        The list of message dicts to be posted to the queue.
+def post(client, queue_url: str, messages: list[dict]) -> dict:
+    """Sends up to 10 articles to the specified SQS queue.
+
+    Takes a list of JSON messages and constructs a list of Entries (dicts with
+    the keys "Id" and "MessageBody") before sending them to the SQS queue with
+    the URL provided.
+
+    Args:
+        client:
+            The botocore.client.SQS instance to use to connect to AWS SQS.
+        queue_url:
+            A string giving the HTTPS endpoint of the SQS queue.
+        messages:
+            The list of message dicts to be posted to the queue.
+    
+    Returns:
+        The response data received by SQS, namely a dict with the keys
+        "ResponseMetadata" and either or both of "Successful" and "Failed".
+        For more information, see https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/sqs/client/send_message_batch.html.
     """
 
     cur_time = datetime.now(UTC)
